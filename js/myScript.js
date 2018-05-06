a1c9cec7aa76f8fb171a8d5d153726ce67a70d8b
“Neha <neha.anandpara1234@gmail.com>
2018-05-06 13:20:34 +0530

    Home page and About Section Created.

---
+++ js/myScript.js
@@ -0,0 +1,100 @@
+
+/*..........For Smooth Scrolling............*/
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+/*...........Initialize wow file.................*/
+	new WOW().init();
+
+/*.............Changing Color on Scrolling....................*/
+
+var $document = $(document),
+	$element = $('.navbar'),
+	navbarDefault = 'navbar-transparent';
+	navbarBlack = 'navbar-onScrolling';
+
+	fadeIn = 'fadeIn';
+
+	$document.scroll(function(){
+		if ($document.scrollTop() >= 100) {
+			$element.addClass(navbarBlack);
+			$element.removeClass(navbarDefault);
+			
+			$element.addClass(fadeIn);
+		}
+		else{
+			$element.addClass(navbarDefault);
+			$element.removeClass(navbarBlack);
+
+			$element.removeClass(fadeIn);
+		}
+	});
+
+
+/*.............Cursor Auto Writing...........................*/
+
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
