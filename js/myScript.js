3b729727d71f15587bc6e3978e49a8029b712038
“Neha <neha.anandpara1234@gmail.com>
2018-05-11 22:15:37 +0530

    front page responsive

--- js/myScript.js
+++ js/myScript.js
@@ -1,7 +1,5 @@
 
-
 /*..........For Smooth Scrolling............*/
-
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
